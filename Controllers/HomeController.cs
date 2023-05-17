a634f0c631c2633202085592bce9aea7417e04dc
AMÉLIE TORRE WALSH <47757058@ort>
2023-05-17 09:02:01 -0300

    Fifth Commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,20 +19,25 @@ public class HomeController : Controller
         return View();
     }
     public IActionResult SelectPaquete(){
+        cargarListas();
+        return View();
+    }
+    void cargarListas(){
         ViewBag.Destinos=ORTWorld.ListaDestinos;
         ViewBag.Hoteles=ORTWorld.ListaHoteles;
         ViewBag.Aerolineas=ORTWorld.ListaAereos;
         ViewBag.Excursiones=ORTWorld.ListaExcursiones;
-        return View();
     }
-    public IActionResult GuardarPaquete(int Destino, int Hotel, int Aereo, int Excursion){
-        if(Destino>0&&Hotel>0&&Aereo>0&&Excursion>0){
-            Paquete nuevo = new Paquete(ORTWorld.ListaHoteles[Hotel],ORTWorld.ListaAereos[Aereo],ORTWorld.ListaExcursiones[Excursion]);
-            ORTWorld.IngresarPaquete(ORTWorld.ListaDestinos[Destino],nuevo);
+    public IActionResult GuardarPaquete(int Destino, string Hotel, string Aereo, string Excursion){
+        if(Destino!=0&&Hotel!=""&&Aereo!=""&&Excursion!=""){
+            Paquete nuevo = new Paquete(Hotel,Aereo,Excursion);
+            ORTWorld.IngresarPaquete(ORTWorld.ListaDestinos[Destino-1],nuevo);
+            ViewBag.Paquetes=ORTWorld.Paquetes;
             return View("Index");
         }
         else{
             ViewBag.ComentarioError="Lo ingresado no es válido. Por favor ingrese opciones del menú";
+            cargarListas();
             return View("SelectPaquete");
         }
     }
--- Models/ORTWorld.cs
+++ Models/ORTWorld.cs
@@ -1,10 +1,9 @@
 class ORTWorld{
     public static List<string> ListaDestinos {get;private set;} = new List<string> (){"Orlando","Paris","Londres","Tokyo","Amsterdam","Florencia","Bariloche","Ottawa","Punta Cana","Seoul"};
-    public static List<string> ListaHoteles {get;private set;} = new List<string> (){"sheraton.jpg","novotel.jpg","marriot.jpg","llaollao.jpg","ibis.jpg","iberostar.jpg","holidayInn.jpg","hilton.jpg","clubMedSandPiper.jpg","4Seasons.jpg"};
-    //descargar imagenes aerolineas
+    public static List<string> ListaHoteles {get;private set;} = new List<string> (){"sheraton.jpg","novotel.jpg","marriot.jpg","llaollao.jpg","ibis.jpg","iberostar.jpg","holidayInn.jpg","hilton.jpg","clubMed.jpg","4Seasons.jpg"};
     public static List<string> ListaAereos {get; private set;} = new List<string> (){"latam.jpg","americanAirlines.jpg","aeromexico.jpg","airCanada.jpg","boa.jpg","airFrance.jpg","british.jpg","iberia.jpg","avianca.jpg","emirates.jpg"};
-    public static List<string> ListaExcursiones {get; private set;} = new List<string> (){"teatro.jpg","ski.jpg","helicoptero.jpg","museo.jpg","tirolesa.jpg","parqueAgua.jpg","buceo.jpg","ciclismo.jpg","canotaje.jpg","trekking.jpg"};
-    public static Dictionary<string, Paquete> Paquetes {get; private set;}
+    public static List<string> ListaExcursiones {get; private set;} = new List<string> (){"teatro.jpg","ski.jpg","helicoptero.jpg","museoVan.jpg","tirolesa.jpg","parqueAgua.jpg","buceo.jpg","ciclismo.jpg","canotaje.jpg","trekking.jpg"};
+    public static Dictionary<string, Paquete> Paquetes {get; private set;} = new Dictionary<string, Paquete>();
 
     public static bool IngresarPaquete(string destinoSeleccionado, Paquete paquete){
         bool coincidencia=false;
