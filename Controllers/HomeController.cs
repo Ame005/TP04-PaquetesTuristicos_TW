7ebdecdbfa013fb98075663bc5ed936caf786510
AMÉLIE TORRE WALSH <47757058@ort>
2023-05-22 09:15:43 -0300

    Delete Button Commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,22 +19,38 @@ public class HomeController : Controller
         return View();
     }
     public IActionResult SelectPaquete(){
+        cargarListas();
+        return View();
+    }
+    void cargarListas(){
         ViewBag.Destinos=ORTWorld.ListaDestinos;
         ViewBag.Hoteles=ORTWorld.ListaHoteles;
         ViewBag.Aerolineas=ORTWorld.ListaAereos;
         ViewBag.Excursiones=ORTWorld.ListaExcursiones;
-        return View();
     }
-    public IActionResult GuardarPaquete(int Destino, int Hotel, int Aereo, int Excursion){
-        if(Destino>0&&Hotel>0&&Aereo>0&&Excursion>0){
-            Paquete nuevo = new Paquete(ORTWorld.ListaHoteles[Hotel],ORTWorld.ListaAereos[Aereo],ORTWorld.ListaExcursiones[Excursion]);
-            ORTWorld.IngresarPaquete(ORTWorld.ListaDestinos[Destino],nuevo);
+    void mensajeError(){
+        ViewBag.ComentarioError="Lo ingresado no es válido. Por favor ingrese opciones del menú";
+    }
+    void cargarORTWPaq(){
+        ViewBag.Paquetes=ORTWorld.Paquetes;
+    }
+    public IActionResult GuardarPaquete(int Destino, string Hotel, string Aereo, string Excursion){
+        if(Destino!=0&&Hotel!=""&&Aereo!=""&&Excursion!=""){
+            Paquete nuevo = new Paquete(Hotel,Aereo,Excursion);
+            ORTWorld.IngresarPaquete(ORTWorld.ListaDestinos[Destino-1],nuevo);
+            cargarORTWPaq();
             return View("Index");
         }
         else{
-            ViewBag.ComentarioError="Lo ingresado no es válido. Por favor ingrese opciones del menú";
+            mensajeError();
+            cargarListas();
             return View("SelectPaquete");
         }
+    }
+    public IActionResult BorrarPaquete(string Destino){
+        ORTWorld.BorrarPaquete(Destino);
+        cargarORTWPaq();
+        return View("Index");
     }
 
     public IActionResult Privacy()
